fd7c52f7ec7a4462f30863a12488c79f2e42650d
giuliomezzotero <mezzoterogiulio9@gmail.com>
2019-11-26 13:29:14 +0100

    Creazione package Utils e funzione per download generico di un file

--- op.project/src/main/java/univpm/op/project/Application.java
+++ op.project/src/main/java/univpm/op/project/Application.java
@@ -3,10 +3,19 @@ package univpm.op.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import univpm.op.project.utils.Utils;
+
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
+		
+		Utils.downloadFile("http://data.europa.eu/euodp/data/api/3/action/package_show?id=GeGKzwDc03b3j0olhD5DQ","Dataset.json");
+
+		
+		
+		//Utils.downloadFile("","Dataset.tsv");
+		
 		SpringApplication.run(Application.class, args);
 	}
 
---
+++ op.project/src/main/java/univpm/op/project/utils/Utils.java
@@ -0,0 +1,46 @@
+package univpm.op.project.utils;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+
+
+public abstract class Utils {
+	
+	public static void downloadFile(String link, String result)
+	{
+		try
+		{
+			byte buffer[] = new byte[2048];
+		    int Rbytes;
+		    
+		    System.out.println("Effettuiamo il download del file");
+			
+			InputStream datasetStream = new URL( link ).openStream();
+			BufferedInputStream inputStream = new BufferedInputStream( datasetStream  );
+			
+			FileOutputStream fileOutputStream = new FileOutputStream( result );
+			
+		    while ((Rbytes = inputStream.read(buffer, 0, 2048)) != -1) {
+		        fileOutputStream.write(buffer, 0, Rbytes);
+		    }
+		    
+		    System.out.println("Download effettuato");
+		    
+		    datasetStream.close();
+		    fileOutputStream.close();
+		    inputStream.close();
+			
+		    
+		} catch (IOException e)
+		
+		{
+			System.err.println("Errore");
+			System.exit(-1);
+		}
+	}
+
+}
