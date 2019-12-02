56f2d46d44ee7d4c29493ba0d247bd956ec9f9c7
User <User@DESKTOP-8P1O9BC>
2019-12-02 17:18:08 +0100

    Sviluppo del metodo metaGet

--- op.project/src/main/java/univpm/op/project/controller/Controller.java
+++ op.project/src/main/java/univpm/op/project/controller/Controller.java
@@ -2,9 +2,17 @@ package univpm.op.project.controller;
 
 import org.json.simple.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
+import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
+
 import univpm.op.project.data.Data;
+import univpm.op.project.entity.Entity;
 
 @RestController
 public class Controller {
@@ -20,4 +28,28 @@ public class Controller {
     {
     	return Data.getJSONData();
     }
+
+    @SuppressWarnings("unchecked")
+	@RequestMapping(value = "/get/metadata", method = RequestMethod.GET, produces="application/json")
+    public String metaGet( )
+    {
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonSchemaGenerator generatorSchema = new JsonSchemaGenerator(objectMapper);
+		JsonSchema schema;
+		try {
+			schema = generatorSchema.generateSchema(Entity.class);
+			return objectMapper.writeValueAsString(schema);
+		} catch (JsonMappingException e) {
+			JSONObject obj = new JSONObject();
+			obj.put("Errore", e.getMessage());
+			return obj.toString();
+		} catch (JsonProcessingException e) {
+			JSONObject obj = new JSONObject();
+			obj.put("Errore", e.getMessage());
+			return obj.toString();
+		}		
+    }
+
 }
+   
+
