4c67967305f430462c6d710bd1267cf90a02b7ca
User <User@DESKTOP-8P1O9BC>
2019-12-02 21:12:12 +0100

    Completamento classe Controller

--- op.project/src/main/java/univpm/op/project/controller/Controller.java
+++ op.project/src/main/java/univpm/op/project/controller/Controller.java
@@ -1,5 +1,10 @@
 package univpm.op.project.controller;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +16,7 @@ import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
 
 import univpm.op.project.data.Data;
 import univpm.op.project.entity.Entity;
+import univpm.op.project.utils.Utils;
 
 @RestController
 public class Controller {
@@ -52,12 +58,48 @@ public class Controller {
 		return Data.DataAnalytic( Data.getData() );
 	}
 	
+	@SuppressWarnings("unchecked")
+	public JSONObject getFilteredAnalytics(String filter)
+	{
+		if( filter == null )
+		{
+			return Data.DataAnalytic( Data.getData() );
+		}
+		
+		List<Entity> data = new ArrayList<Entity>();
+		
+		JSONParser parser = new JSONParser();
+		JSONObject filteredJSON;
+		JSONObject jsonFilters;
+		
+		try {
+			jsonFilters = (JSONObject) parser.parse(filter);
+		} catch (ClassCastException e) {
+			filteredJSON = new JSONObject();
+			filteredJSON.put("WARNING!", "CONFLITTO PARSING RICHIESTA FILTRO");
+			return filteredJSON;
+		} catch (ParseException e) {
+			filteredJSON = new JSONObject();
+			filteredJSON.put("WARNING!", "CONFLITTO PARSING RICHIESTA FILTRO");
+			return filteredJSON;
+		}
 
-
-
-
-
-
+		JSONObject responseValidator = Utils.validFilter( jsonFilters );
+		
+		if( ! responseValidator.containsKey("SUCCESS!") )
+			return responseValidator;
+		
+		
+		
+		
+		for( Entity e: Data.getData() )
+		{
+			if( e.filterApplication( jsonFilters ) )
+				data.add( e );
+		}
+		
+		return Data.DataAnalytic( data );
+	}
 }
    
 
